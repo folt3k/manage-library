708d307f4840d32d976136ab27cd564facedda0a
kfoltyniak <kfoltyniak@software.altkom.pl>
2022-09-18 13:30:13 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,13 @@ git stash
 git fetch
 git checkout main
 git pull
+pm2 delete api
 pm2 kill
+rm -rf ~/.pm2
 rm -rf node_modules
 npm install
 rm -rf ./dist
 npm run build
-pm2 delete api
 pm2 start node ./dist/index.js --name api
 pm2 list all
 sudo service nginx restart
