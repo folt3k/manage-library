fa3b6fb6351e0c82e832c768ce4a0b8d6d987b06
kfoltyniak <kfoltyniak@software.altkom.pl>
2022-09-18 13:19:44 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,11 @@ git stash
 git fetch
 git checkout main
 git pull
+rm -rf node_modules
 npm install
 rm -rf ./dist
 npm run build
+pm2 kill api
 pm2 delete api
 pm2 start node ./dist/index.js --name api
 pm2 list all
