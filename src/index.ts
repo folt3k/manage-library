7a0dcc40692a037097d7463d7ddd6c3d1413a42c
kfoltyniak <kfoltyniak@software.altkom.pl>
2022-09-19 14:47:05 +0200

    init prisma

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const port = 8000;
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+async function main() {
+  await prisma.$connect();
 
-app.listen(port, () => {
-  console.log(`Server is running at https://localhost:${port}`);
-});
+  app.get("/users", async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany();
+
+    res.json(users);
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running at https://localhost:${port}`);
+  });
+}
+
+main();
