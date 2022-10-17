84586b6b8d1795d36a76b12116bb467cb7c29399
folt3k <folt3k@gmail.com>
2022-10-17 20:58:36 +0200

    fix add category

--- src/asset/categories/categories.service.ts
+++ src/asset/categories/categories.service.ts
@@ -2,9 +2,18 @@ import { AssetCategory } from "@prisma/client";
 
 import prisma from "../../../prisma/client";
 import { CreateAssetCategoryDto } from "./categories.types";
+import httpErrors from "../../common/utils/http-error.util";
 
 export const createAssetCategory = async (dto: CreateAssetCategoryDto): Promise<AssetCategory> => {
-  return await prisma.assetCategory.create({ data: dto });
+  const categoryExists = await prisma.assetCategory.findFirst({
+    where: { name: dto.name.toLowerCase() },
+  });
+
+  if (categoryExists) {
+    throw httpErrors.badRequest("Kategoria z taką nazwą już istnieje");
+  }
+
+  return await prisma.assetCategory.create({ data: { name: dto.name.toLowerCase() } });
 };
 
 export const getAssetCategories = async (): Promise<AssetCategory[]> => {
